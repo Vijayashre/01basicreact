e63cf7ceb33ff623ce6e7a2d1784b0c9ebc7c0e7
vijayashre <vkaushiwa@gmail.com>
2023-12-02 17:32:20 +0530

    added file

---
+++ reactbasic/src/Chai.js
@@ -0,0 +1,6 @@
+function Chai(){
+    return(
+        <h1>Chai ki masti</h1>
+    )
+}
+export default Chai;
---
+++ vite-project/src/Vitechai.jsx
@@ -0,0 +1,8 @@
+function Vitechai(){
+    return(
+        
+       <h1>Hello mast wali chai by vite</h1>
+    )
+}
+
+export default Vitechai
